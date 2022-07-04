effcf3826363a00f712e21264daf441811db0372
srajashekhar <udayarajashekhar@gmail.com>
2022-07-04 12:47:04 +0530

    Added: Null pointer Exception

--- src/test/java/com/bridgelabz/jdbc/Day_21_Mood_Analyzer_Programs/MoodAnalyserTest.java
+++ src/test/java/com/bridgelabz/jdbc/Day_21_Mood_Analyzer_Programs/MoodAnalyserTest.java
@@ -1,25 +1,32 @@
 package com.bridgelabz.jdbc.Day_21_Mood_Analyzer_Programs;
 
+import org.junit.Assert;
 import org.junit.Test;
-
-import junit.framework.Assert;
 
 public class MoodAnalyserTest {
 
-	 @Test
-	    public void givenMessage_WhenSad_ShouldReturnSad() throws Exception {
+	@Test
+	public void givenMessage_WhenSad_ShouldReturnSad() throws Exception {
 
-	        MoodAnalyser moodAnalyzer = new MoodAnalyser("This is a Sad message");
-	        String mood = moodAnalyzer.analyseMood();
-	        Assert.assertEquals("SAD",mood);
-	    }
+		MoodAnalyser moodAnalyzer = new MoodAnalyser("This is a Sad message");
+		String mood = moodAnalyzer.analyseMood();
+		Assert.assertEquals("SAD", mood);
+	}
 
-	    @Test
-	    public void givenMessage_WhenHappy_ShouldReturnHappy() throws Exception {
+	@Test
+	public void givenMessage_WhenHappy_ShouldReturnHappy() throws Exception {
 
-	        MoodAnalyser moodAnalyzer = new MoodAnalyser("This is a Happy message");
-	        String mood = moodAnalyzer.analyseMood();
-	        Assert.assertEquals("HAPPY",mood);
-	    }
+		MoodAnalyser moodAnalyzer = new MoodAnalyser("This is a Happy message");
+		String mood = moodAnalyzer.analyseMood();
+		Assert.assertEquals("HAPPY", mood);
+	}
 
+	@Test
+	public void givenMessage_WhenNull_ShouldReturnHappy() {
+
+		MoodAnalyser moodAnalyzer = new MoodAnalyser(null);
+		String mood = moodAnalyzer.analyseMood();
+		Assert.assertEquals("HAPPY", mood);
+
+	}
 }
